711d9c6ec01ed738d47df5d76ba43461fd2fbf8e
xezian <xezian@icloud.com>
2017-11-12 21:59:25 -0700

    basic stuff has been started to create this game which includes pseudocode and some testable click functionality and variables such as an array of objects

---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+// this loads the document first
+$( document ).ready(function() {
+    console.log( "document loaded" );
+// this loads the window first
+    $( window ).on( "load", function() {
+    console.log( "window loaded" );
+// created an array containing all the buddies as objects
+        var buddyArray = [
+            buddyOne = {
+                name: "Schmelbert",
+                type: "Thief",
+                hitPoints: 120,
+                counterPower: 12,
+                attackPower: 8,
+                isDefeated: false
+            },
+            buddyTwo = {
+                name: "Rocky",
+                type: "Sorcerer",
+                hitPoints: 150,
+                counterPower: 18,
+                attackPower: 9,
+                isDefeated: false
+            },
+            buddyThree = {
+                name: "Spike",
+                type: "Fighter",
+                hitPoints: 160,
+                counterPower: 15,
+                attackPower: 11,
+                isDefeated: false
+            },
+            buddyFour = {
+                name: "Fazor",
+                type: "Rogue",
+                hitPoints: 180,
+                counterPower: 20,
+                attackPower: 15,
+                isDefeated: false
+            },
+            buddyFive = {
+                name: "King Loren",
+                type: "King",
+                hitPoints: 190,
+                counterPower: 16,
+                attackPower: 7,
+                isDefeated: false
+            }
+        ];
+// created variables to represent states of the game
+        var battlePossible = false;   
+// created a start button we can use to start the game
+        var startButton = $("<button>");
+        startButton.addClass("btn start-button btn-warning");
+        startButton.text("START");
+        $("#start").append(startButton);
+// created an on click event to start the whole game!
+        $(".start-button").on("click", function() {        
+// created and appended buttons to the document representing the buddies from the array
+            for (var i = 0; i < buddyArray.length; i++) {
+                var newDiv = $("<div>");
+                newDiv.addClass("row")
+                newDiv.attr("id", buddyArray[i].type);
+                var buddyButton = $("<button>");
+                buddyButton.addClass("btn buddy-button btn-info w-100");
+                buddyButton.attr("data-hp", buddyArray[i].hitPoints);
+                buddyButton.html(buddyArray[i].name + "<br>" + buddyArray[i].hitPoints);
+                $("#buddybank").append(newDiv);
+                $(`#${buddyArray[i].type}`).append(buddyButton);
+            }
+// remove start button ;)
+            $("#message").empty();  
+            $("#start").empty();     
+// created a button for attack
+
+// created on click events to handle the attack being clicked
+
+// created on click events to handle the buddy buttons being clicked
+
+// 
+        });
+    });
+});
+
+
