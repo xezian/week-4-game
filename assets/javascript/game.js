ca4e641375b5d077e5dd09bc4ca0a2b0cdeffb02
xezian <xezian@icloud.com>
2017-11-13 18:52:02 -0700

    got the basic button functionality working to the point of selecting one buddy, and all other buddies go to the defender pool which can then be selected as opponents

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,36 +7,36 @@ $( document ).ready(function() {
 // created an array containing all the buddies as objects
         var buddyArray = [
             buddyOne = {
-                name: "Schmelbert",
-                type: "Thief",
+                name: "Schmilbert",
+                type: "thief",
                 hitPoints: 120,
                 counterPower: 12,
                 attackPower: 8
             },
             buddyTwo = {
                 name: "Rocky",
-                type: "Sorcerer",
+                type: "sorcerer",
                 hitPoints: 150,
                 counterPower: 18,
                 attackPower: 9
             },
             buddyThree = {
                 name: "Spike",
-                type: "Fighter",
+                type: "fighter",
                 hitPoints: 160,
                 counterPower: 15,
                 attackPower: 11
             },
             buddyFour = {
                 name: "Fazor",
-                type: "Rogue",
+                type: "rogue",
                 hitPoints: 180,
                 counterPower: 20,
                 attackPower: 15
             },
             buddyFive = {
                 name: "King Loren",
-                type: "King",
+                type: "king",
                 hitPoints: 190,
                 counterPower: 16,
                 attackPower: 7
@@ -50,33 +50,51 @@ $( document ).ready(function() {
         startButton.text("START");
         $("#start").append(startButton);
 // created an on click event to start the whole game!
-        $(".start-button").on("click", function() {        
+        $(document).on("click", ".start-button", function() {        
 // created and appended buttons to the document representing the buddies from the array
             for (var i = 0; i < buddyArray.length; i++) {
                 var newDiv = $("<div>");
-                newDiv.addClass("row")
+                newDiv.addClass("row buddyrow");
                 newDiv.attr("id", buddyArray[i].type);
                 var buddyButton = $("<button>");
                 buddyButton.addClass("btn buddy-button btn-info w-100");
                 buddyButton.attr("data-hp", buddyArray[i].hitPoints);
                 buddyButton.attr("data-ap", buddyArray[i].attackPower);
                 buddyButton.attr("data-cp", buddyArray[i].counterPower);
+                buddyButton.attr("name", buddyArray[i].name)
                 buddyButton.html(buddyArray[i].name + "<br>" + buddyArray[i].hitPoints);
                 $("#buddybank").append(newDiv);
                 $(`#${buddyArray[i].type}`).append(buddyButton);
             }
 // remove start button ;)
             $("#message").empty();  
-            $("#start").empty();    
-// created event listeners to on click events for the buddy buttons
-            
+            $("#start").empty();          
+// created event listener to on click events for the buddy buttons
+            $(document).on("click", ".buddy-button", function () {
+// this is where the setup happens for the game. the buddy you click gets appended to the arena <div id="buddy">, and all the other buddies go into the defender pool. <div id="enemybuddies">.
+
+// I started by replacing my alert with a confirm
+                var buddyChosen = confirm("do you choose " + $(this).attr("name") + "?");
+                if (buddyChosen) {
+                    $(this).removeClass("buddy-button").addClass("chosen-buddy");
+                    $(this).appendTo("#buddy");
+                    $(".buddy-button").removeClass("buddy-button btn-info").addClass("enemy-button btn-danger");
+                    $(".enemy-button").appendTo("#enemybuddies");
+                    }
+                });
+// created event listener to on click events for the enemy buttons
+                $(document).on("click", ".enemy-button", function () {
+                    var enemyChosen = confirm("OK, " + $(".chosen-buddy").attr("name") + ". Will you fight " + $(this).attr("name") + "?");
+                    if (enemyChosen) {
+                        $(this).removeClass("enemy-button").addClass("opponent");
+                        $(this).appendTo("#opponent");
+                };
+            });
 // created a button for attack
 
 // created on click events to handle the attack being clicked
 
 // created on click events to handle the buddy buttons being clicked
-
-// 
         });
     });
 });
